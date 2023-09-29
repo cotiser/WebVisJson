fc6a9ecb0c544c5809878efc77e7cfbda6c98b0b
keeser <1326658417@qq.com>
2023-09-30 03:39:49 +0800

    实现了3d表初步显示

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -15,24 +15,25 @@
       </el-row>
     </el-card>
 
-    <el-row style="margin-top: 60px">
-      <el-col :span="16">
-        <div id="main" style="width: 1000px; height: 600px"></div>
+    <el-row>
+      <el-col :span="56">
+        <div id="main" style="width: 1000px; height: 700px"></div>
       </el-col>
-      <el-col :span="6"> </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
 // 1.导入echarts
-import echarts from 'echarts'
+import * as echarts from 'echarts'
+import 'echarts-gl'
 
 export default {
   data() {
     return {
       fileName: null,
-      jsonData: null
+      // jsonData: null,
+      scatterData: []
     }
   },
   methods: {
@@ -50,7 +51,19 @@ export default {
           reader.onload = (e) => {
             try {
               const jsonContent = JSON.parse(e.target.result)
-              this.jsonData = JSON.stringify(jsonContent, null, 2)
+              // 遍历data
+              jsonContent.data.forEach((item) => {
+                this.scatterData.push([
+                  item.mmwave_data[0].x,
+                  item.mmwave_data[0].y,
+                  item.mmwave_data[0].z
+                ])
+              })
+              // setTimeout(() => {
+              //   // 创建散点图并设置动画效果
+              //   this.createScatter3DChart()
+              // }, 1000) // 模拟加载延迟
+              // this.jsonData = JSON.stringify(jsonContent, null, 2)
             } catch (error) {
               console.error('JSON解析错误:', error)
             }
@@ -65,72 +78,65 @@ export default {
   },
   // 此时,页面上的元素,已经被渲染完毕了
   async mounted() {
-    // 3.基于准备好的dom，初始化echarts实例
+    //createScatter3DChart() {
+    // 初始化echarts实例
     var myChart = echarts.init(document.getElementById('main'))
-    // 4.准备数据项和配置项
-    // 指定图表的配置项和数据
-    var option = {
-      legend: {},
-      tooltip: {
-        trigger: 'axis',
-        showContent: false
-      },
-      dataset: {
-        source: [
-          ['product', '2012', '2013', '2014', '2015', '2016', '2017'],
-          ['Matcha Latte', 41.1, 30.4, 65.1, 53.3, 83.8, 98.7],
-          ['Milk Tea', 86.5, 92.1, 85.7, 83.1, 73.4, 55.1],
-          ['Cheese Cocoa', 24.1, 67.2, 79.5, 86.4, 65.2, 82.5],
-          ['Walnut Brownie', 55.2, 67.1, 69.2, 72.4, 53.9, 39.1]
-        ]
-      },
-      xAxis: { type: 'category' },
-      yAxis: { gridIndex: 0 },
-      grid: { top: '55%' },
-      series: [
-        { type: 'line', smooth: true, seriesLayoutBy: 'row' },
-        { type: 'line', smooth: true, seriesLayoutBy: 'row' },
-        { type: 'line', smooth: true, seriesLayoutBy: 'row' },
-        { type: 'line', smooth: true, seriesLayoutBy: 'row' },
-        {
-          type: 'pie',
-          id: 'pie',
-          radius: '30%',
-          center: ['50%', '25%'],
-          label: {
-            formatter: '{b}: {@2012} ({d}%)'
-          },
-          encode: {
-            itemName: 'product',
-            value: '2012',
-            tooltip: '2012'
-          }
-        }
-      ]
-    }
 
-    myChart.on('updateAxisPointer', function (event) {
-      var xAxisInfo = event.axesInfo[0]
-      if (xAxisInfo) {
-        var dimension = xAxisInfo.value + 1
-        myChart.setOption({
-          series: {
-            id: 'pie',
-            label: {
-              formatter: '{b}: {@[' + dimension + ']} ({d}%)'
-            },
-            encode: {
-              value: dimension,
-              tooltip: dimension
+    // 准备数据项和配置项
+    // const data = [
+    //   [6.5423455238342285, 0.8681341409683228, 2.162891149520874],
+    //   [3.5882620811462402, 1.1126402616500854, 1.23966646194458],
+    //   [3.5893783569335938, 1.1002776622772217, 1.1001211404800415]
+    //   // 添加更多数据点...
+    // ]
+    // 指定图表的配置项和数据
+    while (this.scatterData.length != 0) {
+      await new Promise((resolve) => setTimeout(resolve, 1000)) // 异步等待 3 秒
+      console.log(this.scatterData)
+      var option = {
+        tooltip: {},
+        xAxis3D: { name: 'X' },
+        yAxis3D: { name: 'Y' },
+        zAxis3D: { name: 'Z' },
+        grid3D: {},
+        series: [
+          {
+            type: 'scatter3D', // 使用散点图
+            data: this.scatterData.shift(),
+            symbolSize: 10, // 点的大小
+            animarion: true, // 启动动画效果
+            itemStyle: {
+              color: 'blue' // 点的颜色
             }
           }
-        })
+        ]
       }
-    })
+      // 展示数据
+      myChart.setOption(option)
+    }
+
+    // 鼠标事件处理
+    // myChart.on('updateAxisPointer', function (event) {
+    //   var xAxisInfo = event.axesInfo[0]
+    //   if (xAxisInfo) {
+    //     var dimension = xAxisInfo.value + 1
+    //     myChart.setOption({
+    //       series: {
+    //         id: 'pie',
+    //         label: {
+    //           formatter: '{b}: {@[' + dimension + ']} ({d}%)'
+    //         },
+    //         encode: {
+    //           value: dimension,
+    //           tooltip: dimension
+    //         }
+    //       }
+    //     })
+    //   }
+    // })
+
     // 数据合并
     //  const result = _.merge(res.data, this.options)
-    // 5.展示数据
-    myChart.setOption(option)
   }
 }
 </script>
